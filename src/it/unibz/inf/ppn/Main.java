f4a47e761da16fecb53dc25d0791a62cd2a5bb82
Philipp Neugebauer <ppn@nexgo.de>
2014-01-07 22:14:46 +0100

    error handling for invalid parameters added

--- src/it/unibz/inf/ppn/Main.java
+++ src/it/unibz/inf/ppn/Main.java
@@ -3,6 +3,12 @@ package it.unibz.inf.ppn;
 public class Main {
 
 	public static void main(String[] args) {
-		new PathSearcher(args[0]);
+		if (args.length == 1) {
+			new PathSearcher(args[0]);
+		} else {
+			System.err
+					.println("Invalid parameter. Enter the folder of the repositories.");
+			System.exit(1);
+		}
 	}
 }
