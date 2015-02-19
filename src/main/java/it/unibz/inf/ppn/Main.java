e38cdd5076c91626ad33201a34136c1afa38d481
Philipp Neugebauer <philippneugebauer@users.noreply.github.com>
2015-02-19 16:36:57 +0100

    improved order in if statement

--- src/main/java/it/unibz/inf/ppn/Main.java
+++ src/main/java/it/unibz/inf/ppn/Main.java
@@ -22,7 +22,7 @@ package it.unibz.inf.ppn;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 1 && args != null && !args[0].isEmpty()) {
+        if (args != null && args.length == 1 && !args[0].isEmpty()) {
             new PathSearcher(args[0]);
         } else {
             System.err
